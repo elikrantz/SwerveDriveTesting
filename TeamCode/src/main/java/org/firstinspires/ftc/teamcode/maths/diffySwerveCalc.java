3990c6ee3ef37dff808dfa31e7776895a93ae43f
24ekrantz <elijkrantz@gmail.com>
2024-02-23 17:03:09 -0500

    me lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/maths/diffySwerveCalc.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/maths/diffySwerveCalc.java
@@ -1,4 +1,8 @@
 package org.firstinspires.ftc.teamcode.maths;
 
 public class diffySwerveCalc {
+    public double calculute(String motorLocation, double wheelVel, double moduleAngle) {
+
+        return 10;
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/maths/swerveCalc.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/maths/swerveCalc.java
@@ -1,11 +1,24 @@
 package org.firstinspires.ftc.teamcode.maths;
 
+import org.firstinspires.ftc.teamcode.RobotConstants;
+
 import java.util.Arrays;
 
 public class swerveCalc {
+    public double[] calculate(int moduleNum,double velRobotX,double velRobotY,double velRobotRot) {
+        double velModuleX;
+        double velModuleY;
 
-    public double[] calculate() {
-        double[] test = new double[] {};
-        return test;
+        double velWheel;
+        double moduleAngle;
+
+        velModuleX = (1 * velRobotX) + (0 * velRobotY) + (-RobotConstants.modulePositions[moduleNum][1] * velRobotRot);
+        velModuleY = (0 * velRobotX) + (1 * velRobotY) + (-RobotConstants.modulePositions[moduleNum][0] * velRobotRot);
+
+        velWheel = Math.sqrt(Math.pow(velModuleX,2) + Math.pow(velModuleY,2));
+        moduleAngle = Math.atan2(velModuleY,velModuleX);
+
+        double[] output = new double[] {velWheel, moduleAngle};
+        return output;
     }
 }
